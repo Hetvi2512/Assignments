71095c5a7f80ef8d320c85d6a81d1cce344258d7
Hetvi <hetvishah2512@gmail.com>
2020-11-26 00:15:30 +0530

    Added folder Structure

---
+++ Registration_form/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Registration_form.Models;
+
+namespace Registration_form.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            userinfo userModel = new userinfo();
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult Add(userinfo userModel)
+        {
+            using(DbModels dbmodel = new DbModels())
+            {
+                if(dbmodel.userinfoes.Any(x=>x.Email == userModel.Email))
+                {
+                    ViewBag.DuplicateMessage = "User Name already Exists";
+                    return View("Index", userModel);
+                }
+                dbmodel.userinfoes.Add(userModel);
+                dbmodel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successfull";
+            return View("Index", new userinfo());
+        }
+    }
+}
---
+++ Registration_form/CustomValidations/CityAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registration_form.CustomValidations
+{
+    public class CityAttribute:ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid
+    (object value, ValidationContext validationContext)
+        {
+            string city = value.ToString();
+
+            if (city != "Rajkot" && city != "Mumbai" && city != "Delhi")
+            {
+                return new ValidationResult("Invalid city, Valid values are Rajkot, Mumbai, and Delhi.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
