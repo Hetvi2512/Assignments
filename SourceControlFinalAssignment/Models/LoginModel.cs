d14c7b9ae68771596445925879d09fe30b80a4d8
Hetvi <hetvishah2512@gmail.com>
2020-12-20 22:45:38 +0530

    Added Source control final Assignment

---
+++ SourceControlFinalAssignment/Models/LoginModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SourceControlFinalAssignment.Models
+{
+    [NotMapped]
+    public class LoginModel
+    {
+        [DisplayName("Email-Id")]
+        [Required(ErrorMessage = "This Field is required")]
+        [RegularExpression(@"\b[\w\.-]+@[\w\.-]+\.\w{2,4}\b", ErrorMessage = "Not a Valid Email-Id")]
+        public string email { get; set; }
+
+
+        [DisplayName("Password")]
+        [StringLength(50, MinimumLength = 8, ErrorMessage = "Minimum 8 Characters required")]
+        [Required(ErrorMessage = "This Field is required")]
+        [DataType(DataType.Password)]
+        public string password { get;  set; }
+
+    }
+}
---
+++ SourceControlFinalAssignment/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using SourceControlFinalAssignment.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SourceControlFinalAssignment
+{
+    public class MyDbContext : DbContext
+    {
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
